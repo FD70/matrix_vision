3479ae56ab9efbbb738a8ba9dd68bface9df6f70
FD70000000 <halfl.ez.ser@mail.ru>
2022-05-25 10:24:09 +0300

    check gh works

--- main.py
+++ main.py
@@ -7,6 +7,20 @@
 
 # [-] Добавить возможность изменять цвет фона, символов реалтайм
 
+#FIXME:
+def video_capture():
+    return
+
+
+#FIXME:
+def video_to_text():
+    return
+
+
+#FIXME:
+def text_to_frame():
+    return
+
 
 if __name__ == '__main__':
     print('[+] запуск из main.py')
